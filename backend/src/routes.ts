4f32509b1275b1345713624e99aaac2d90bcfac3
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-10-14 10:57:43 -0300

    edit category

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,8 +2,11 @@ import { Router } from "express";
 
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { checkAdminUser } from "./middlewares/checkAdminUser";
+
 import { AuthUserController } from "./controllers/user/AuthUserController";
+
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
+import { PatchCategoryController } from "./controllers/category/PatchCategoryController";
 
 const router = Router();
 
@@ -12,6 +15,7 @@ router.get("/user/auth", checkAdminUser)
 router.post("/user/auth", new AuthUserController().handle);
 
 // CATEGORY ROUTES
-router.post("/category/create", isAuthenticated, new CreateCategoryController().handle)
+router.post("/category/create", isAuthenticated, new CreateCategoryController().handle);
+router.put("/category/edit", isAuthenticated, new PatchCategoryController().handle);
 
 export { router };
