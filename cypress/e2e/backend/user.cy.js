6f227666fdda0ac7aeead4ed4204e502380827ea
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-10-28 10:51:34 -0300

    starting automated tests

---
+++ cypress/e2e/backend/user.cy.js
@@ -0,0 +1,69 @@
+describe('user routes testing', () => {
+  context('GET /user/auth', () => {
+    it('should return create or user admin already exists', () => {
+      cy.request({
+        method: 'GET',
+        url: 'http://localhost:3333/user/auth'
+      })
+      .should((response) => {
+        expect(response.status).to.eq(200);
+        cy.log(JSON.stringify(response.body));
+      });
+    });
+  });
+
+  context('POST /user/auth', () => {
+    it('should return 200 and authenticated', () => {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/user/auth',
+        body: {
+          "email": "admin@email.com",
+          "password": "admin123"
+        }
+      })
+      .should((response) => {
+        expect(response.status).to.eq(200);
+        cy.log(JSON.stringify(response.body));
+        const token = response.body.token;
+        cy.log(token);
+      })
+    });
+  });
+
+  context('POST /user/auth', () => {
+    it('should return status code 401 when sending wrong email', () => {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/user/auth',
+        body: {
+          "email": "admin@wrong.com",
+          "password": "admin123"
+        },
+        failOnStatusCode: false
+      })
+      .should((response) => {
+        expect(response.status).to.eq(401);
+        expect(response.body).to.eq('User/Password is incorrect');
+      });
+    });
+  });
+
+  context('POST /user/auth', () => {
+    it('should return status code 401 when sending wrong password', () => {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:3333/user/auth',
+        body: {
+          "email": "admin@email.com",
+          "password": "wrongpass"
+        },
+        failOnStatusCode: false
+      })
+      .should((response) => {
+        expect(response.status).to.eq(401);
+        expect(response.body).to.eq('User/Password is incorrect');
+      });
+    });
+  });
+});
