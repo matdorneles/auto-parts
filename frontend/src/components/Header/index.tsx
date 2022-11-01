0f8e524129794f7f66e024f9802696bf08cf124c
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-11-01 10:55:21 -0300

    starting frontend w react

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+function Header() {
+  return (
+    <header>
+      <h1>AutoParts</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
