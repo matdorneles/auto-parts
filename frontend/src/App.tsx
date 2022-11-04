24ebc18da5a435095e2746b8d8bf15a759c727b1
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-11-04 10:53:33 -0300

    start working on home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css';
-import Header from "./components/Header";
+
+import Home from "./views/Home";
 
 function App() {
   return (
-    <Header />
+    <div className='App'>
+      <Home />
+    </div>
   );
 }
 
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,9 +1,21 @@
-import './style.css'
+import './style.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons"
+import { faGear } from "@fortawesome/free-solid-svg-icons"
 
 function Header() {
+  const whatsappIcon = <FontAwesomeIcon icon={faWhatsapp} />
+  const gearIcon = <FontAwesomeIcon icon={faGear} />
+
   return (
     <header>
-      <h1>AutoParts</h1>
+      <a href='/' className='header-logo'>
+        <h1>{gearIcon} AutoParts</h1>
+      </a>
+      <a href='/' className='header-item'>Home</a>
+      <a href='/products' className='header-item'>Produtos</a>
+      <a href='/contact' className='header-item'>Contato</a>
+      <a href='/whatsapp' className='header-item'>{whatsappIcon}</a>
     </header>
   );
 }
