a036686bf09cd1abbeb4ea14539902aa563ed6c7
Matheus Dorneles <matheus.sdorneles1@gmail.com>
2022-11-14 13:19:54 -0300

    initiating CSS mobile first

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -2,7 +2,6 @@ import './style.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons"
 import { faGear, faBars } from "@fortawesome/free-solid-svg-icons"
-//import { faBars } from "@fortawesome/free-regular-svg-icons";
 
 function Header() {
   const whatsappIcon = <FontAwesomeIcon icon={faWhatsapp} />
@@ -13,9 +12,9 @@ function Header() {
     <header>
       <i>{menuIcon}</i>
       <a href='/' className='logo'>
-        <h3><i>{gearIcon}</i> Logo</h3>
+        <span><i>{gearIcon}</i> Logo</span>
       </a>
-      <a href='/whatsapp' className='header-item'><i>{whatsappIcon}</i></a>
+      <a href='/whatsapp'><i>{whatsappIcon}</i></a>
     </header>
   );
 }
